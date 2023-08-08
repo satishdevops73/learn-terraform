c62da1c4aa72b753eb9b833aaf731e562015d9d6
Satish <satishbabu.uppula14@gmail.com>
2023-08-08 08:31:47 +0530

    ec2 creation along with security group creation 4

--- 02-ec2/securitygroup.tf
+++ 02-ec2/securitygroup.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "Test" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ sg-0aac959b0d3a9dedc ]
